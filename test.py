646e63f77ff160f38d8d63fefabe95c21e56eca7
Nicolai Schlüter <Schlueter71@outlook.com>
2021-11-01 15:34:36 +0100

    HTTP-Server Get Request

--- test.py
+++ test.py
@@ -1,16 +1,31 @@
 from http.server import BaseHTTPRequestHandler,HTTPServer
-import time
 
 HOST = "127.0.0.1"
-PORT = 80
+PORT = 8080
 
 class RequestHandler(BaseHTTPRequestHandler):
 
-    def post(self):
-        pass
+    def do_GET(self):
+        
+        try:
+            html = open("index.html", "rb")
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            self.wfile.write(html.read())
+
+            html.close()
+            return
+
+        except IOError:
+            self.send_error(404, "Piss dich!")
 
 
 if __name__ == "__main__ ":
-    print("[Starting the server]")
-    
-    http_server = HTTPServer((HOST, PORT), RequestHandlerClass)
+    print("[Server] wird gestartet...")
+    http_server = HTTPServer((HOST, PORT), RequestHandler)
+    print("[Server] ist gestartet...")
+
+    http_server.serve_forever()
