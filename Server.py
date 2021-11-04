3793d245c979e75eeabf80bf012992cf7a4d175e
Nicolai Schlüter <Schlueter71@outlook.com>
2021-11-04 15:31:24 +0100

    POST-Request sends Json back

--- Server.py
+++ Server.py
@@ -2,56 +2,77 @@ from http.server import BaseHTTPRequestHandler,HTTPServer
 import io
 import PIL.Image as Image
 import cgi
+import base64
+import json
+import logging
 
 HOST = "127.0.0.1"
 PORT = 8080
 
 class RequestHandler(BaseHTTPRequestHandler):
 
-    def _set_response(self):
+    def _set_headers(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "application/json")
         self.end_headers
 
+    def do_HEAD(self):
+        self._set_headers()
+
     def do_GET(self):
+        logging.warning("**GET**")
+        logging.warning(self.headers)
         
-        try:
-            html = open("index.html", "rb")
-            self.wfile.write(html.read())
-            html.close()
+        json_string = json.dumps({
+                'Hallo': 'DU SOLLST EINE POST-REQUEST MACHEN!'
+            })
 
-            self._set_response()
-
-        except IOError:
-            self.send_error(404, "file not found")
-            
+        self._set_headers()
+        self.wfile.write(json_string.encode(encoding='utf-8'))     
 
     def do_POST(self):
-        
+        logging.warning("**POST**")
+        logging.warning(self.headers)
+
         ctype, pdict = cgi.parse_header(self.headers["Content-Type"])
 
-        if ctype == "multipart/form-data":
-            pdict["boundary"] = bytes(pdict["boundary"], "utf-8")
-            form = cgi.parse_multipart(self.rfile, pdict)
+        if ctype != "multipart/form-data":
+            self.send_error(400, "is not in a multipart/form-data")
+            self.end_headers()
+            return
+            
+        pdict["boundary"] = bytes(pdict["boundary"], "utf-8")
+        form = cgi.parse_multipart(self.rfile, pdict)
         
         b = form.get("image")
         
         try:
-            img = Image.open(io.BytesIO(b[0]))
+            img = Image.open(io.BytesIO(base64.b64decode(b)))
             img.show()
+            img.save("1.png")
 
-            self._set_response()
+            json_string = json.dumps({
+                'status': 'successful',
+                'solution': 'Hier steht eine Lösung!'
+            })
+
+            self._set_headers()
+            self.wfile.write(json_string.encode(encoding='utf-8'))
+
+            return
 
         except IOError:
             self.send_error(415, "file-extension is not allowed")
 
-def run():
-    print("HTTP-Server is starting...")
-    http_server = HTTPServer((HOST, PORT), RequestHandler)
-    print("HTTP-Server is running...")
+def runserver():
+    try:
+        print("HTTP-Server is starting...")
+        http_server = HTTPServer((HOST, PORT), RequestHandler)
+        print("HTTP-Server is running...")
 
-    http_server.serve_forever()
+        http_server.serve_forever()
 
+    except KeyboardInterrupt:
+        http_server.server_close()
 
-if __name__ == '__main__':
-    run()
+runserver()
